edf717e9bbd5fcc84882d400cc74cff6b5b129bb
Robert <rrsadykov_1@edu.hse.ru>
2023-03-03 01:30:36 +0300

    add

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from instagrapi import Client
 from config import USER, PASS
-class InstaBot:
-    cl=Client()
-    cl.login(USER, PASS)
-    PATH=''
-    CAPTION='test'
-    def main(self):
-        self.cl.clip_upload(self.PATH,self.CAPTION)
-    def run(self):
-        self.main()
+import os
+
+# dir_capt = caption
+
+for i in os.listdir("clips"):
+    class InstaBot:
+        cl = Client()
+        cl.login(USER, PASS)
+        PATH = 'clips'
+        CAPTION = ''
+
+        def main(self):
+            self.cl.clip_upload(self.PATH, self.CAPTION)
+
+        def run(self):
+            self.main()
 
 if __name__ == '__main__':
-    bot=InstaBot()
+    bot = InstaBot()
     bot.run()
