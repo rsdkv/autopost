d6f461df2c80a24e02cc33c2d4f617155a7ba030
Садыков Роберт Радикович <rrsadykov_1@edu.hse.ru>
2023-03-03 06:06:43 +0300

    worked

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-USER=''
-PASS=''
+USER = 'film927363'
+PASS = 'film927363!'
+
+# user_name = 'film927363'
+# password = 'film927363!'
+
+# film_production513
+# Film2211!
--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 from instagrapi import Client
 from config import USER, PASS
+import time
+import random
 import os
 
 # dir_capt = caption
+r = (random.randint(90, 200))
 
-for i in os.listdir("clips"):
-    class InstaBot:
-        cl = Client()
-        cl.login(USER, PASS)
-        PATH = 'clips'
-        CAPTION = ''
 
-        def main(self):
-            self.cl.clip_upload(self.PATH, self.CAPTION)
 
-        def run(self):
-            self.main()
+class InstaBot:
+    cl = Client()
+    cl.login(USER, PASS)
+    def main(self):
+        for i in os.listdir('C:\\Users\\Robert\\PycharmProjects\\autopost\\clips'):
+            print(os.listdir("clips"))
+            print(f'firstprint_{i}')
+            PATH = f'C:\\Users\\Robert\\PycharmProjects\\autopost\\clips\\{i}'.strip()
+            print(f'take C:\\Users\\Robert\\PycharmProjects\\autopost\\clips\\{i}')
+            CAPTION = f'test{i}'.strip()
+            print(f'caption test_{i}')
+            print(f'secondprint_{i}')
+            self.cl.clip_upload(PATH, CAPTION)
+            time.sleep(r)
+    def run(self):
+        self.main()
 
 if __name__ == '__main__':
     bot = InstaBot()
     bot.run()
+# C:\Users\Robert\PycharmProjects\autopost\clips\first_file.mp4
+#
+#
+# class InstaBot:
+#     cl = Client()
+#     cl.login(USER, PASS)
+#     for i in os.listdir('C:\\Users\\Robert\\PycharmProjects\\autopost\\clips'):
+#         print(os.listdir("clips"))
+#         print(f'first_{i}')
+#         PATH = f'C:\\Users\\Robert\\PycharmProjects\\autopost\\clips\\{i}'.strip()
+#         print(f'take C:\\Users\\Robert\\PycharmProjects\\autopost\\clips\\{i}')
+#         CAPTION = f'test{i}'.strip()
+#         print(f'caption test_{i}')
+#         print(f'second_{i}')
+#     def main(self):
+#         self.cl.clip_upload(self.PATH, self.CAPTION)
+#         time.sleep(r)
+#     def run(self):
+#         self.main()
