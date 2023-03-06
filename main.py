871eb749572c1fd77e63d709fe7395f4f7e6a1b3
Садыков Роберт Радикович <rrsadykov_1@edu.hse.ru>
2023-03-06 04:01:09 +0300

    worked

--- main.py
+++ main.py
@@ -4,27 +4,35 @@ import time
 import random
 import os
 
+path = 'C:\\Users\\Robert\\PycharmProjects\\autopost\\test2\\'
+
 # dir_capt = caption
 r = (random.randint(90, 200))
-
 
 
 class InstaBot:
     cl = Client()
     cl.login(USER, PASS)
+
     def main(self):
-        for i in os.listdir('C:\\Users\\Robert\\PycharmProjects\\autopost\\clips'):
-            print(os.listdir("clips"))
+        for i in os.listdir(path):
+            # print(os.listdir(path))
             print(f'firstprint_{i}')
-            PATH = f'C:\\Users\\Robert\\PycharmProjects\\autopost\\clips\\{i}'.strip()
-            print(f'take C:\\Users\\Robert\\PycharmProjects\\autopost\\clips\\{i}')
-            CAPTION = f'test{i}'.strip()
-            print(f'caption test_{i}')
+            PATH = path + f'{i}'.strip()
+            print('take' + path + f'{i}')
+            # print(f'take C:\\Users\\Robert\\PycharmProjects\\autopost\\test2\\{i}')
+            # CAPTION = f'Название фильма ищите в телеграм канале\n\n\n\n\nКод фильма:{i}'.strip()
+            CAPTION = 'Название фильма ищите в телеграм канале\n' + '.\n' + '.\n' + '.\n' + '.\n' + '.\n' + '.\n' + '.\n' + '.\n' + f'Код фильма:{i}'.replace(
+                '.mp4', '')
+            print(f'Описание фильма которое публикуется:\nНазвание фильма ищите в телеграм канале\n Код фильма:{i}')
             print(f'secondprint_{i}')
             self.cl.clip_upload(PATH, CAPTION)
             time.sleep(r)
+            # цикл удаляющий .jpg в отдельный файл
+
     def run(self):
         self.main()
+
 
 if __name__ == '__main__':
     bot = InstaBot()
